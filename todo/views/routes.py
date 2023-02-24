e06f7b0f100e9bb73fe8bcb6ef724c544ec3acda
Jonoym <86467852+Jonoym@users.noreply.github.com>
2023-02-24 05:42:19 +0000

    Updated deadline, updated and created at times

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -14,8 +14,8 @@ def get_todos():
         "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
         "completed": True,
         "deadline_at": "2023-02-27T00:00:00",
-        "created_at": "2023-02-27T00:00:00",
-        "updated_at": "2023-02-27T00:00:00",
+        "created_at": "2023-02-20T00:00:00",
+        "updated_at": "2023-02-20T00:00:00",
     }])
 
 @api.route('/todos/<int:id>', methods=['GET'])
@@ -26,8 +26,8 @@ def get_todo(id):
         "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
         "completed": True,
         "deadline_at": "2023-02-27T00:00:00",
-        "created_at": "2023-02-27T00:00:00",
-        "updated_at": "2023-02-27T00:00:00",
+        "created_at": "2023-02-20T00:00:00",
+        "updated_at": "2023-02-20T00:00:00",
     })
 
 @api.route('/todos', methods=['POST'])
@@ -62,6 +62,6 @@ def delete_todo(id):
         "description": "Watch the CSSE6400 lecture on ECHO360 for Week 1",
         "completed": True,
         "deadline_at": "2023-02-27T00:00:00",
-        "created_at": "2023-02-27T00:00:00",
-        "updated_at": "2023-02-27T00:00:00",
+        "created_at": "2023-02-20T00:00:00",
+        "updated_at": "2023-02-20T00:00:00",
     })
